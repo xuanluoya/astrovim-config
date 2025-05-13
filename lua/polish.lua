84532db58b341e30b514cb6a89b711c5fe7a9700
Aislin <72391548+Xuanluo-Qiu@users.noreply.github.com>
2025-05-14 03:01:51 +0800

    fix neovide copy/cut

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,12 +1,25 @@
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
-vim.o.guifont = "Maple Mono NF:h14"
+
+-- Neovide Config
+vim.o.guifont = "RecMonoCasual Nerd Font:h14"
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.experimental_layer_grouping = true
 vim.g.neovide_fullscreen = true
+vim.g.neovide_macos_simple_fullscreen = true
 vim.g.neovide_cursor_vfx_mode = "torpedo"
 
+if vim.g.neovide then
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+end
+
+-- Allow clipboard copy paste in neovim
 vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
